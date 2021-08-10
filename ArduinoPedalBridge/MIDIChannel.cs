9751cfc13fd1a9c9e29502aa43a3e21b52be173f
Brc <No_No_And_No>
2021-08-10 13:38:16 +0200

    Full chain now works. Everything still in prototype stage

---
+++ ArduinoPedalBridge/MIDIChannel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoPedalBridge
+{
+  enum MIDIChannel
+  {
+    C1 = 0x0,
+    C2 = 0x1,
+    C3 = 0x2,
+    C4 = 0x3,
+    C5 = 0x4,
+    C6 = 0x5,
+    C7 = 0x6,
+    C8 = 0x7,
+    C9 = 0x8,
+    C10 = 0x9,
+    C11 = 0xA,
+    C12 = 0xB,
+    C13 = 0xC,
+    C14 = 0xD,
+    C15 = 0xE,
+    C16 = 0xF,
+  }
+}
